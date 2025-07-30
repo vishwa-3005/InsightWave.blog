e09bacca3566bbcd522a8963f57043335a2aa74d
vishwadeep sankpal <vishwadeep108@gmail.com>
2025-07-30 23:13:29 +0530

    Implement AuthService class for user authentication with Appwrite

---
+++ insightWave/src/appwrite/auth.js
@@ -0,0 +1,64 @@
+import config from "../conf/config.js";
+
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      if (userAccount) {
+        //if userAccount is created then login directly
+
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await account.get();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async logout() {
+    try {
+      return await account.deleteSessions();
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+const authService = new AuthService();
+export default authService;
